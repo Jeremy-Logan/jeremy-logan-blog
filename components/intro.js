2b992f75b66baaec93d371d2d35809cc1c2b9885
Jeremy-Logan <44982128+Jeremy-Logan@users.noreply.github.com>
2021-03-07 17:05:25 -0800

    added portfolio page

--- components/intro.js
+++ components/intro.js
@@ -1,14 +1,25 @@
 import { CMS_NAME, CMS_URL } from '../lib/constants'
+import Link from 'next/link'
 
 export default function Intro() {
-  return (
-    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Think Jeremy Logan
-      </h1>
-      <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        My Creative Works and Musings.
-      </h4>
-    </section>
-  )
+	return (
+		<section className='flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12'>
+			<h1 className='text-5xl md:text-7xl font-bold tracking-tighter leading-tight md:pr-8'>
+				Think Jeremy Logan
+			</h1>
+			<h4 className='text-center md:text-left text-lg mt-5 md:pl-8'>
+				A Page for My{' '}
+				<button className='bg-blue-200 shadow-md py-2 px-3 rounded-md border-2 border-blue-600 transition duration-300 ease-in-out hover:scale-125 hover:shadow-lg hover:bg-blue-300'>
+        <Link href='/portfolio'> 
+					Creative Works
+          </Link>
+				</button>{' '}
+				and{' '}
+				<button className='bg-blue-200 shadow-md py-2 px-3 rounded-md border-2 border-blue-600 transition duration-300 ease-in-out hover:scale-125 hover:shadow-lg hover:bg-blue-300'>
+					{' '}
+					<Link href='/#blog-hero'>Musings</Link>
+				</button>
+			</h4>
+		</section>
+	)
 }
---
+++ pages/portfolio.js
@@ -0,0 +1,15 @@
+import Header from '../components/header'
+
+const Portfolio =() =>{
+    return(
+        <div>
+        <Header/>
+        <h1>Portfolio</h1>
+        </div>
+        
+        
+        
+        )
+}
+
+export default Portfolio
