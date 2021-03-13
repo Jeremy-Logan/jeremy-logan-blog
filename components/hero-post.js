dc9a8035c0a7498056900ee7408cb8ceaa64fb33
Jeremy-Logan <44982128+Jeremy-Logan@users.noreply.github.com>
2021-03-13 13:00:09 -0800

    added exerpt to hero-blog

--- components/hero-post.js
+++ components/hero-post.js
@@ -2,36 +2,57 @@ import Avatar from '../components/avatar'
 import Date from '../components/date'
 import CoverImage from '../components/cover-image'
 import Link from 'next/link'
+import BlockContent from '@sanity/block-content-to-react'
+import markdownStyles from './markdown-styles.module.css'
 
 export default function HeroPost({
-  title,
-  coverImage,
-  date,
-  excerpt,
-  author,
-  slug,
+	title,
+	coverImage,
+	date,
+	excerpt,
+	author,
+	slug,
 }) {
-  return (
-    <section>
-      <div className="mb-8 md:mb-16">
-        <CoverImage slug={slug} imageObject={coverImage} title={title} url={coverImage} id='blog-hero' />
-      </div>
-      <div className="md:grid md:grid-cols-2 md:col-gap-16 lg:col-gap-8 mb-20 md:mb-28">
-        <div>
-          <h3 className="mb-4 text-4xl lg:text-6xl leading-tight">
-            <Link as={`/posts/${slug}`} href="/posts/[slug]">
-              <a className="hover:underline">{title}</a>
-            </Link>
-          </h3>
-          <div className="mb-4 md:mb-0 text-lg">
-            <Date dateString={date} />
-          </div>
-        </div>
-        <div>
-          <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
-          <Avatar name={author?.name} picture={author?.picture} />
-        </div>
-      </div>
-    </section>
-  )
+	return (
+		<section>
+			<div className='mb-8 md:mb-16'>
+				<CoverImage
+					slug={slug}
+					imageObject={coverImage}
+					title={title}
+					url={coverImage}
+					id='blog-hero'
+				/>
+			</div>
+			<div className='md:grid md:grid-cols-2 md:col-gap-16 lg:col-gap-8 mb-20 md:mb-28'>
+				<div>
+					<h3 className='mb-4 text-4xl lg:text-6xl leading-tight'>
+						<Link as={`/posts/${slug}`} href='/posts/[slug]'>
+							<a className='hover:underline'>{title}</a>
+						</Link>
+					</h3>
+
+					<div>
+						<div className='flex flex-row mb-6 text-lg justify-between items-center'>
+							<Avatar
+              
+								name={author?.name}
+								picture={author?.picture}
+							/>
+							<Date dateString={date} className=''/>
+						</div>
+
+						<BlockContent
+							blocks={excerpt}
+							projectId={
+								process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
+							}
+							dataset={process.env.NEXT_PUBLIC_SANITY_DATASET}
+							className={markdownStyles.markdown}
+						/>
+					</div>
+				</div>
+			</div>
+		</section>
+	)
 }
--- components/intro.js
+++ components/intro.js
@@ -3,23 +3,37 @@ import Link from 'next/link'
 
 export default function Intro() {
 	return (
-		<section className='flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12'>
-			<h1 className='text-5xl md:text-7xl font-bold tracking-tighter leading-tight md:pr-8'>
-				Think Jeremy Logan
-			</h1>
-			<h4 className='text-center md:text-left text-lg mt-5 md:pl-8'>
-				A Page for My{' '}
-				<button className='bg-yellow-200 shadow-md py-2 px-3 rounded-sm transition duration-300 ease-in-out hover:shadow-lg hover:bg-yellow-300'>
-        <Link href='/portfolio'> 
-					Creative Works
-          </Link>
-				</button>{' '}
-				and{' '}
-				<button className='bg-yellow-200 shadow-md py-2 px-3 rounded-sm transition duration-300 ease-in-out hover:shadow-lg hover:bg-yellow-300'>
-					{' '}
-					<Link href='/#blog-hero'>Musings</Link>
-				</button>
-			</h4>
-		</section>
+		<>
+			<section className='flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12 '>
+				<h1 className='text-5xl md:text-7xl font-bold tracking-tighter leading-tight md:pr-8'>
+					Think Jeremy Logan
+				</h1>
+				<div className='mx-10 md:mx-0'>
+					<h4 className='text-center md:text-left text-lg mt-5 md:pl-8'>
+						A Page for My{' '}
+						<button className='bg-yellow-200 shadow-md py-2 px-3 mx-2 rounded-sm transition duration-300 ease-in-out hover:shadow-lg hover:bg-yellow-300'>
+							<Link href='/portfolio'>Creative Works</Link>
+						</button>{' '}
+						and{' '}
+						<button className='bg-yellow-200 shadow-md py-2 px-3 my-4 mx-2 rounded-sm transition duration-300 ease-in-out hover:shadow-lg hover:bg-yellow-300'>
+							{' '}
+							<Link href='/#blog-hero'>Musings</Link>
+						</button>
+					</h4>
+				</div>
+			</section>
+			<section>
+				<div className='divide-y-2 divide-solid divide-gray-300'>
+					<div></div>
+					<p className='py-6 text-lg'>
+						Hello, my name is Jeremy Logan. I'm a designer,
+						illustrator, animator, and programmer.<br/> In this site
+						you'll find some of my latest creations as well as my
+						thoughts on creativity and coding.
+					</p>
+					<div className='mb-12'></div>
+				</div>
+			</section>
+		</>
 	)
 }
